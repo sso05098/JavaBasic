5ae1d2485a38978822ab3dae0cb3bb33e8c66152
sso05098 <162398884+sso05098@users.noreply.github.com>
2024-03-06 18:15:50 +0900

    Add files via upload

---
+++ print01.java
@@ -0,0 +1,14 @@
+package sec04;
+
+public class print01{
+	public static void main(String[]args) {
+		String myName="홍길동";
+		System.out.println("이름:" + myName);
+		
+		int myAge=20;
+		System.out.println("나이:" + myAge);
+		System.out.println();
+		System.out.println("이름:" + myName + "   나이 : "+myAge);
+		
+	}
+}
