3eb0c1d017a6d6b287ce8b4f2fc7efa2eda7568a
sso05098 <162398884+sso05098@users.noreply.github.com>
2024-05-02 23:21:02 +0900

    Add files via upload

---
+++ 도전과제/GirlTest.java
@@ -0,0 +1,12 @@
+package 도전과제;
+
+public class GirlTest {
+    public static void main(String[] args) {
+        Girl g1 = new Girl();
+        Girl g2 = new GoodGirl();
+        GoodGirl gg = new BestGirl();
+
+        gg.show(); // 이 부분을 주석 처리 해제 후 실행
+    }
+}
+
